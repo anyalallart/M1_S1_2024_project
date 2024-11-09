a445031bbb0ab43f6c7a926a02a844c1b42a5969
camilleLanger <camille.langer@student.junia.com>
2024-11-09 15:43:31 +0100

    suite back

--- m1-api/src/app.module.ts
+++ m1-api/src/app.module.ts
@@ -2,8 +2,8 @@
 
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ControllerModule } from './controllers/controller.module'; // Importe le module des contrôleurs
-import { Book } from './entities/book.entity'; // Assure-toi que le chemin est correct
+import { ControllerModule } from './controllers/controller.module';
+import { Book } from './entities/book.entity';
 
 
 
