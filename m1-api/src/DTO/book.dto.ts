07af595c38992c684adfe71179cee8728208fb84
anyalallart <anya.lallart@student.junia.com>
2024-11-10 18:48:55 +0100

    ajout prix dans le back

--- m1-api/src/DTO/book.dto.ts
+++ m1-api/src/DTO/book.dto.ts
@@ -2,5 +2,6 @@ export class CreateBookDto {
     title: string;
     publicationDate: string;
     author: string;
+    price: number;
 }
 
--- m1-api/src/models/book.model.ts
+++ m1-api/src/models/book.model.ts
@@ -1,12 +1,11 @@
 import { BookId } from '../entities/book.entity'; //rajouter page auhthor
 import { ratingModel } from './Rating.model';
 
-
-
 export class BookModel {
     id: BookId;
     title: string;
     publicationDate: string;
     author: string; //mettre author
+    price: number;
     rating?: ratingModel[];
 }
